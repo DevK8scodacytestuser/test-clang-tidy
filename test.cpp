542c67954974a004229e6716d30c96af751daac5
DevK8scodacytestuser <59879235+DevK8scodacytestuser@users.noreply.github.com>
2020-04-15 18:49:19 +0100

    Simplify test file

--- test.cpp
+++ test.cpp
@@ -5,13 +5,6 @@ void f(int i) {
     // Exception is created but is not thrown.
     std::runtime_error("Unexpected argument");
   }
-  // Original - Addition in the integer domain
-  int x;
-  absl::Time t;
-  int result = absl::ToUnixSeconds(t) + x;
-
-  // Suggestion - Addition in the absl::Time domain
-  int result = absl::ToUnixSeconds(t + absl::Seconds(x));
 }
 
 int main() { return 0; }
